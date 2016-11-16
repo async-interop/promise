765a6f696f72dd63da21826e78fc466e2bdf44a0
Niklas Keller <me@kelunik.com>
2016-11-16 11:40:48 +0100

    Remove ::class usage, as it's not available in PHP 5.4

--- src/Promise/ErrorHandler.php
+++ src/Promise/ErrorHandler.php
@@ -109,15 +109,13 @@ final class ErrorHandler
         }
 
         if (empty(self::$callbacks)) {
-            trigger_error(sprintf(
-                "An exception has been thrown from an %s::when handler, but no handler has been registered via %s::add."
-                . " At least one handler has to be registered to prevent exceptions from going unnoticed. Do NOT"
-                . " install an empty handler that just does nothing. If the handler is called, there is ALWAYS"
-                . " something wrong.\n\n%s",
-                Promise::class,
-                ErrorHandler::class,
-                (string) $error
-            ), E_USER_ERROR);
+            trigger_error(
+                "An exception has been thrown from an Interop\\Async\\Promise::when handler, but no handler has been"
+                . " registered via Interop\\Async\\Promise\\ErrorHandler::add. At least one handler has to be"
+                . " registered to prevent exceptions from going unnoticed. Do NOT install an empty handler that just"
+                . " does nothing. If the handler is called, there is ALWAYS something wrong.\n\n" . (string) $error,
+                E_USER_ERROR
+            );
         }
     }
 }
